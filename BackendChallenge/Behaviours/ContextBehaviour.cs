78e28c9d4dff539027e1c997318b239c576cd587
Matheus Henrique Gomes Ferreira <mhferreira.14@outlook.com>
2023-10-29 17:31:48 -0300

    Configure Behaver to save the context

---
+++ BackendChallenge/Behaviours/ContextBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using BackendChallenge.infra.Database;
+using MediatR;
+
+namespace BackendChallenge.Behaviours
+{
+    public class ContextBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly DatabaseContext _context;
+
+        public ContextBehaviour(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var result = await next();
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return result;
+        }
+    }
+}
