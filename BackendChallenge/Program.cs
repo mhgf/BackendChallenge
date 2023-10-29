22819217d3bbd908cd1f5facdc184e7353413b9a
Matheus Henrique Gomes Ferreira <mhferreira.14@outlook.com>
2023-10-29 17:36:03 -0300

    Configuration the application

--- BackendChallenge/Program.cs
+++ BackendChallenge/Program.cs
@@ -1,18 +1,41 @@
-using Newtonsoft.Json;
+using BackendChallenge.Behaviours;
+using BackendChallenge.core.Commands.User;
+using BackendChallenge.core.Entity;
+using BackendChallenge.core.Notification;
+using BackendChallenge.core.Repositories;
+using BackendChallenge.core.Services;
+using BackendChallenge.Filters;
+using BackendChallenge.infra.Database;
+using BackendChallenge.infra.Database.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<NotificationContext>();
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+// Entity Framework
+builder.Services.AddEntityFrameworkNpgsql();
+builder.Services.AddNpgsql<DatabaseContext>(builder.Configuration.GetConnectionString("Database"));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+// Add services to the container.
+builder.Services.AddControllers(opt => opt.Filters.Add<NotificationFilter>()).AddNewtonsoftJson();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddMediatR(cfg => {
+
+builder.Services.AddMediatR(cfg =>
+{
     cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    cfg.AddOpenBehavior(typeof(ContextBehaviour<,>));
 });
+
+builder.Services.AddScoped<IRequestHandler<CreateUserCommand, User?>, UserCreateHandler>();
 
 var app = builder.Build();
 
